17a72ec3b60837ec1770589736888d1afb8ab625
AliceKass <alice.kassinger@gmail.com>
2016-07-18 16:56:01 -0600

    wrote conditional statements
    Wrote if, else if, and else statements. they don’t work for some
    reason.

---
+++ index.js
@@ -0,0 +1,33 @@
+// Pseudo-Code
+// 	Note: if button inside the form, 
+		//the click of the button and the enter keypress will trigger form submission
+
+// Load JS when $(document).ready;
+// On form submission, ($click)
+	// override default for form submission
+	// get value of input
+	// store that value as temp variable var city
+	// Change background image depending on value of city
+//End
+
+$(document).ready(function(){
+
+function handleCitySearch(event) {
+	//prevent default behavior of form submission (new page)
+	event.preventDefault();
+
+	//get value of input and store it
+	var city = ($('#city-type').val());
+
+	//conditional statements
+	if (city === 'SF' || 'San Francisco') {
+		$('body').css('background', 'url(../images/sf.jpg');
+	} else if (city === 'LA' || 'Los Angeles') {
+		$('body').css('.la');
+	} else (city === 'AT' || Austin) {
+		$('body').css('background', 'url(../images/austin.jpg');
+	} 
+}
+
+	$('#search').submit(handleCitySearch);
+});
